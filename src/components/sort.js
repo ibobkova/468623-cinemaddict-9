34d5856b9aeb77bc953a02127a06dd4e4bce3a94
Irina Bobkova <goodgrinko@gmail.com>
2019-08-12 18:13:04 +0300

    feat (Components): added sort && filled card, menu, popup and search

---
+++ src/components/sort.js
@@ -0,0 +1,15 @@
+import {createElement} from "../utils";
+
+const markup = `
+  <ul class="sort">
+    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
+    <li><a href="#" class="sort__button">Sort by date</a></li>
+    <li><a href="#" class="sort__button">Sort by rating</a></li>
+  </ul>
+`;
+
+const createSort = () => {
+  return createElement(markup);
+};
+
+export default createSort;
---
+++ src/data.js
@@ -0,0 +1,83 @@
+export const movies = [
+  {
+    title: `The Dance of Life`,
+    rating: `8.3`,
+    year: `1929`,
+    duration: `1h 55m`,
+    genre: `Musical`,
+    desc: `Burlesque comic Ralph "Skid" Johnson (Skelly), and specialty dancer Bonny Lee King (Carroll), end up together on a cold, rainy night at a tr…`,
+    comments: []
+  },
+  {
+    title: ``,
+    rating: ``,
+    year: ``,
+    duration: ``,
+    genre: ``,
+    desc: ``,
+    comments: []
+  },
+  {
+    title: ``,
+    rating: ``,
+    year: ``,
+    duration: ``,
+    genre: ``,
+    desc: ``,
+    comments: []
+  },
+  {
+    title: ``,
+    rating: ``,
+    year: ``,
+    duration: ``,
+    genre: ``,
+    desc: ``,
+    comments: []
+  },
+  {
+    title: ``,
+    rating: ``,
+    year: ``,
+    duration: ``,
+    genre: ``,
+    desc: ``,
+    comments: []
+  },
+  {
+    title: ``,
+    rating: ``,
+    year: ``,
+    duration: ``,
+    genre: ``,
+    desc: ``,
+    comments: []
+  },
+  {
+    title: ``,
+    rating: ``,
+    year: ``,
+    duration: ``,
+    genre: ``,
+    desc: ``,
+    comments: []
+  },
+  {
+    title: ``,
+    rating: ``,
+    year: ``,
+    duration: ``,
+    genre: ``,
+    desc: ``,
+    comments: []
+  },
+  {
+    title: ``,
+    rating: ``,
+    year: ``,
+    duration: ``,
+    genre: ``,
+    desc: ``,
+    comments: []
+  },
+];
